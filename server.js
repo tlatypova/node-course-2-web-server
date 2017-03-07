db2737e4713be924eaaa7a7471944e66f5d68518
Tse <verbena70@comcast.net>
2017-03-07 14:22:37 -0500

    Portfolio is added

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: "404"

d6b0e250176a6ab46e1f02827707af7d70f65975
Tse <verbena70@comcast.net>
2017-03-12 20:38:43 -0400

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -30,6 +32,24 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e);
     });
 
+});
+
+// GET /todos/12334
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo
+    .findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      return res.send({todo});
+    }, () => {
+      return res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
